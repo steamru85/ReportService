1fbe476b44ae128c9cfe637097b743bca0a5f12a
Rustem Musairov <steamru@ya.ru>
2018-05-21 10:00:25 +0500

    passwordstore

---
+++ ReportService/ReportService/Domain/PasswordStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportService.Domain
+{
+    public class PasswordStore
+    {
+        public string UserName { get => "admin"; }
+        public string Password { get => "pa$$w0rd"; }
+    }
+}
