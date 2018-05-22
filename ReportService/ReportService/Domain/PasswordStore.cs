acd7b9b4b71aa40e896b501949a9822a1a658c9e
Rustem Musairov <steamru@ya.ru>
2018-05-22 17:21:03 +0500

    ну теперь то должен ругнуться

--- ReportService/ReportService/Domain/PasswordStore.cs
+++ ReportService/ReportService/Domain/PasswordStore.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,7 +14,17 @@ namespace ReportService.Domain
         public PasswordStore()
         {
             Employee employee = null;
+            Employee employee2 = null;
             Console.WriteLine(employee.Name);
+            Console.WriteLine(employee2.Name);
+            File.OpenRead("C:\\1.txt");
+        }
+
+        public int SomeMetdod(bool add, int c1, int c2)
+        {
+            if (add)
+                return c1 + c2;
+            else return c1 - c2;
         }
     }
 }
