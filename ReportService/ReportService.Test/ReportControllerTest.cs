cc562185f155c4a65151199d0eb847e1cf14d4ee
skotina <lemeshev.k.v@gmail.com>
2018-06-04 14:57:32 +0500

    Добавлен тестовый проект

---
+++ ReportService/ReportService.Test/ReportControllerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReportService.Test
+{
+    [TestClass]
+    public class ReportControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
