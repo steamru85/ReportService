d9449ccf6023a46f3100cec146847c4279492432
skotina <lemeshev.k.v@gmail.com>
2018-06-05 08:57:05 +0500

    По аналогии с Department разбор ридера перенесен в конструктор

--- ReportService/ReportService/Domain/Employee.cs
+++ ReportService/ReportService/Domain/Employee.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace ReportService.Domain
 {
     public class Employee
     {
+        public Employee() { }
+
+        public Employee(NpgsqlDataReader reader)
+        {
+            Name = reader.GetString(0);
+            Inn = reader.GetString(1); Department = reader.GetString(2);
+        }
+
         public string Name { get; set; }
         public string Department { get; set; }
-        public string  Inn { get; set; }
+        public string Inn { get; set; }
         public int Salary { get; set; }
         public string BuhCode { get; set; }
     }
